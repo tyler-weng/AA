aa652ae0f86e1dd18787b8a7001876a5c762288e
Tyler Weng <tylercweng@gmail.com>
2017-02-04 15:04:22 -0800

    add w2d5

---
+++ w2/w2d5/hash_map_lru_cache/lib/p01_int_set.rb
@@ -0,0 +1,105 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max + 1, false)
+    @max = max
+  end
+
+  def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num].dup
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @max)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num) if self.include?(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    unless self.include?(num)
+      self[num] << num
+      @count += 1
+      resize! if @count > num_buckets
+    end
+  end
+
+  def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) }
+  end
+
+end
